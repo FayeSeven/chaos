f54311fca3924a029cae0517cd84883a119c37a3
FayeSeven <fayeseven@163.com>
2018-05-22 14:56:24 +0800

    css渐变色背景

--- src/views/Here.vue
+++ src/views/Here.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         这里
+        <div class="here"></div>
     </div>
 </template>
 
@@ -11,5 +12,13 @@
 </script>
 
 <style scoped>
-
+    .here{
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        opacity: .3;
+        background: linear-gradient(to right, rgb(69, 104, 220), rgb(176, 106, 179));
+    }
 </style>
